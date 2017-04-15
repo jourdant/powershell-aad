393528fa32ad6298a3119f59f462eb76f7009308
Jourdan Templeton <jourdan.templeton@awaregroup.com>
2017-04-15 13:34:30 +1200

    Added strong typing to functions

--- aad.psm1
+++ aad.psm1
@@ -1,3 +1,12 @@
+##
+
+Class AadAccessToken {
+    [datetime]$Expiry
+    [string]$Resource
+    [string]$TokenType
+    [string]$Value
+}
+
 function Get-AadAccessToken ($ClientID, $ClientSecret, $TenantID, $Resource) {
     $uri = "https://login.microsoftonline.com/$TenantID/oauth2/token"
     $headers = @{ Accepts="application/json" }
@@ -9,13 +18,20 @@ function Get-AadAccessToken ($ClientID, $ClientSecret, $TenantID, $Resource) {
     }    
 
     $token = Invoke-RestMethod -Uri $uri -Body $body -ContentType "application/x-www-form-urlencoded" -Method Post -Headers $headers
-    return $token
+    $ret = New-Object AadAccessToken -Prop @{
+        Expiry=([datetime]"1970-01-01 00:00:00").AddSeconds($token.expires_on);
+        Resource=$token.resource;
+        TokenType=$token.token_type;
+        Value=$token.access_token;
+    }
+
+    return $ret
 }
 
-function Invoke-AadWebRequest ($Url, $Authorization, $Method="Get", $Body=$null) {
+function Invoke-AadWebRequest ($Url, [AadAccessToken]$AadAccessToken, $Method="Get", $Body=$null) {
     $headers = @{ 
         Accepts="application/json";
-        Authorization="Bearer $($Authorization.access_token)";
+        Authorization="Bearer $($Authorization.value)";
     }
 
     return Invoke-RestMethod -Uri $url -Method $Method -Headers $headers -Body $body
